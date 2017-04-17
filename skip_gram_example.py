757948dc52df9d8bed7dc7fbd3f0c184c3ffe2ce
Rachel Song (Kyuye) <ivyheart2@gmail.com>
2017-04-17 11:39:16 +0900

    rename

--- skip_gram_example.py
+++ skip_gram_example.py
@@ -1,15 +1,15 @@
 """
-* this is the skip gram example
+* this is the skip gram practice code
 """
 
 import tensorflow as tf
 
 CONSTANT = tf.app.flags
-CONSTANT.DEFINE_integer("dict_size", 50000, "dictionary size")
-CONSTANT.DEFINE_integer("embed_size", 200, "embeded dictionary size")
-CONSTANT.DEFINE_integer("num_sampled", 64, "num sampled for nce loss function")
-CONSTANT.DEFINE_integer("learning_rate", 1e-4, "learning rate")
-CONSTANT.DEFINE_integer("epoch", 100, "epoch for training")
+CONSTANT.DEFINE_integer("dict_size", 50000, "")
+CONSTANT.DEFINE_integer("embed_size", 200, "")
+CONSTANT.DEFINE_integer("num_sampled", 64, "")
+CONSTANT.DEFINE_integer("learning_rate", 1e-4, "")
+CONSTANT.DEFINE_integer("epoch", 100, "")
 CONST = CONSTANT.FLAGS
 
 
@@ -17,7 +17,7 @@ class SkipGram(object):
     """
      * skip gram class
     """
-    def __init__(self):
+    def init(self):
         self._build_skipgram()
         self._initialization()
 
@@ -41,34 +41,32 @@ class SkipGram(object):
         print embeddings[0]
         self._close_session()
 
-    @classmethod
-    def _run_train(cls):
-        _, loss = cls.sess.run([cls.optimizer, cls.loss])
+
+    def run_train(self):
+        _, loss = self.sess.run([self.optimizer, self.loss])
         return loss
 
-    @classmethod
-    def _run_prediction(cls):
-        embeddings = cls._get_embeddings()
-        return cls.sess.run(embeddings)
+    def run_prediction(self):
+        embeddings = self._get_embeddings()
+        return self.sess.run(embeddings)
 
-    @classmethod
-    def _gen_onehots(cls):
+  
+    def gen_onehots(self):
         indices = tf.cast(tf.transpose([range(CONST.dict_size), range(CONST.dict_size)]), tf.int64)
         values = tf.ones((CONST.dict_size), dtype=tf.int64)
         shape = (CONST.dict_size, CONST.dict_size)
         st_onehots = tf.SparseTensor(indices=indices, values=values, shape=shape)
-        cls.ts_onehots = tf.sparse_tensor_to_dense(st_onehots)
+        self.ts_onehots = tf.sparse_tensor_to_dense(st_onehots)
 
-    @classmethod
-    def _set_variables(cls):
-        cls.embed_dict = tf.Variable(tf.random_uniform((CONST.dict_size, CONST.embed_size)))
+        
+    def set_variables(self):
+        self.embed_dict = tf.Variable(tf.random_uniform((CONST.dict_size, CONST.embed_size)))
 
-    @classmethod
-    def _build_skipgram(cls):
+    def build_skipgram(self):
         sz_weights = (CONST.dict_size, CONST.embed_size)
 
         init_embeddings = tf.random_uniform(sz_weights, -1, 1)
-        cls.embeddings = tf.Variable(init_embeddings)
+        self.embeddings = tf.Variable(init_embeddings)
 
         stddev = 1.0/tf.sqrt(tf.cast(CONST.embed_size, tf.float32))
         init_nce_weight = tf.truncated_normal(sz_weights, stddev=stddev)
@@ -79,9 +77,9 @@ class SkipGram(object):
 
         x_word = tf.constant([1, 2, 3, 4, 5])
         y_word = tf.constant([[4], [5], [6], [7], [8]])
-        embeds = tf.nn.embedding_lookup(cls.embeddings, x_word)
+        embeds = tf.nn.embedding_lookup(self.embeddings, x_word)
 
-        cls.loss = tf.nn.nce_loss(
+        self.loss = tf.nn.nce_loss(
             weights=w_nce,
             biases=nce_bias,
             inputs=embeds,
@@ -90,21 +88,21 @@ class SkipGram(object):
             num_classes=CONST.dict_size
         )
 
-        cls.optimizer = tf.train.GradientDescentOptimizer(CONST.learning_rate).minimize(cls.loss)
+        self.optimizer = tf.train.GradientDescentOptimizer(CONST.learning_rate).minimize(self.loss)
 
-    @classmethod
-    def _get_embeddings(cls):
-        norm = tf.sqrt(tf.reduce_sum(tf.square(cls.embeddings), 1, keep_dims=True))
-        return cls.embeddings/norm
+   
+    def get_embeddings(self):
+        norm = tf.sqrt(tf.reduce_sum(tf.square(self.embeddings), 1, keep_dims=True))
+        return self.embeddings/norm
 
-    @classmethod
-    def _initialization(cls):
-        cls.sess = tf.Session()
-        cls.sess.run(tf.global_variables_initializer())
+    
+    def initialization(self):
+        self.sess = tf.Session()
+        self.sess.run(tf.global_variables_initializer())
 
-    @classmethod
-    def _close_session(cls):
-        cls.sess.close()
+    
+    def close_session(self):
+        self.sess.close()
 
 def main(_):
     """
