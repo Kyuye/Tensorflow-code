bd56e8374ea6e179decf09ef888a638410e484eb
leechanwoo <cjswosa22@naver.com>
2017-07-11 16:07:11 +0900

    distributed learning updated (file load)

--- sources/local.sh
+++ sources/local.sh
@@ -1,6 +1,6 @@
 gcloud ml-engine local train \
 --package-path=$(pwd)/sources/personality_replication \
---module-name=personality_replication.model_single \
-# --distributed \
-# --parameter-server-count=5 \
-# --worker-count=10
+--module-name=personality_replication.model_dist \
+--distributed \
+--parameter-server-count=1 \
+--worker-count=1
--- sources/submit.sh
+++ sources/submit.sh
@@ -2,7 +2,7 @@ job_name=wgan_rn_$(date +%Y%m%d_%H%M%S)
 
 gcloud ml-engine jobs submit training $job_name \
 --package-path=$(pwd)/sources/personality_replication \
---module-name=personality_replication.model \
+--module-name=personality_replication.model_single \
 --staging-bucket=gs://tensorflowprojects-mlengine/ \
 --region=europe-west1 \
 --scale-tier=BASIC_GPU \
